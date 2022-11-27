ee5374621719cd437c6d48bd9bd79f070fa7cd0c
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2022-11-27 14:28:51 -0500

    Add files via upload

---
+++ SideStoreJitterbugPair.bat
@@ -0,0 +1,28 @@
+@ECHO off
+
+REM Jitterbug.bat - A batch script to run jitterbugpair.exe, and convert its output to a SideStore compatible file type
+
+REM ===== Basic Instructions ==========================================================
+ECHO Please connect your iPhone/iPad to your computer via USB cable, and unlock it.
+ECHO Press the spacebar to continue  . . . 
+Pause > nul
+
+REM ===== Jitterbug first run - Will probably fail due to Trust prompt ================
+jitterbugpair /verbose
+ECHO.
+ECHO Press the spacebar to attempt to pair again  . . . 
+Pause > nul
+
+REM ===== Jitterbug second run - Will probably succeed ================================
+jitterbugpair /verbose
+
+REM ===== Renaming .mobiledevicepairing to .plist
+ECHO Renaming
+RENAME *.mobiledevicepairing *.plist
+FOR /f %%i in ('DIR *.plist /b') do set pair=%%i
+ECHO.
+ECHO %pair% has been created
+REM ===== Script ends, user exits the prompt ==========================================
+ECHO Press the spacebar to exit  . . . 
+Pause > nul
+exit
